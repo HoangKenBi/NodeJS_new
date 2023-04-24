8a6d5a16df4ab2cf887cc57abafea939295f7a14
admin <HoangTHTH2202002@fot.edu.vn>
2023-04-24 20:48:31 +0700

    commit

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,5 @@ let userController = require("../controllers/user.controller");
 router.get("/",userController.get);
 router.get("/create-user",userController.createForm);
 router.post("/create-user",userController.save);
-// router.get("/edit-student/:id",userController.editForm);
-// router.post("/edit-student/:id",userController.update);
-// router.post("/delete-student/:id",userController.delete);
 
 module.exports = router;
