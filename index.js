b71ca6c503852267a2e371f1d0ed13cb88354d60
admin <HoangTHTH2202002@fot.edu.vn>
2023-04-24 19:59:46 +0700

    commit

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+// connect mongodb
+const database = require("./src/database");
+const app = express();
+// start session
+const session = require("express-session");
+app.use(session({
+    resave: true,
+    saveUninitialized:true,
+    secret: "t2203e",
+    cookie: {
+        maxAge: 3600000, // miliseconds
+        // secure: true
+    },
+}))
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log("Server is running...");
+})
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",function (req,res){
+    let student = {
+        name: "Nguyễn Văn An",
+        age: 19
+    };
+    let classRoom = {
+        name: "T2203E",
+        room: "B14"
+    }
+    res.render("home",{
+        student: student,
+        classRoom: classRoom
+    });
+});
